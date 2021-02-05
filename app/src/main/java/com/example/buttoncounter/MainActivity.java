7c430e16f1f5e3a006a4b1d44de3d5f3f9863722
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-02-05 02:16:25 -0500

    added resource strings and rotation state management

--- app/src/main/java/com/example/buttoncounter/MainActivity.java
+++ app/src/main/java/com/example/buttoncounter/MainActivity.java
@@ -3,54 +3,36 @@ package com.example.buttoncounter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Bundle stateBundle = new Bundle();
     private buttonIncre incrementalCounter;
-    private Button upButton;
-    private Button downButton;
     private TextView viewCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        upButton = (Button) this.findViewById(R.id.upButton);
-        downButton = (Button) this.findViewById(R.id.downButton);
         viewCount = (TextView) this.findViewById(R.id.count);
         incrementalCounter = new buttonIncre();
         viewCount.setText(String.valueOf(incrementalCounter.increment));
 
     }
-    //When Rotate do...
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setContentView(R.layout.activity_main);
-        }
-        else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-
-        }
-    }
     //Restore state of app after change from saved state
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        savedInstanceState.getBundle("stateBundle");
+        incrementalCounter.increment = savedInstanceState.getInt("count");
+        viewCount.setText(String.valueOf(incrementalCounter.increment));
     }
     //Save state of app prior to change
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt("count", incrementalCounter.increment);
         super.onSaveInstanceState(outState);
-        outState.putBundle("stateBundle", stateBundle);
+
     }
 
     @Override
@@ -58,8 +40,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v.getId() == R.id.upButton) {
             incrementalCounter.up();
         }
-        else {
+        else if(v.getId() == R.id.downButton){
             incrementalCounter.down();
+        }
+        else if(v.getId() == R.id.resetButton) {
+            incrementalCounter.reset();
         }
         viewCount.setText(String.valueOf(incrementalCounter.increment));
     }
@@ -68,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         private int increment;
 
         private buttonIncre() {
-            int increment = 0;
+            increment = 0;
         }
 
         private int up() {
@@ -76,6 +61,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         private int down() {
             return this.increment--;
+        }
+        private void reset() {
+            this.increment = 0;
         }
     }
 }
