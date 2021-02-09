824a0bbaa57792c6930f022614b864f754d75fa2
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2021-02-09 00:19:11 -0500

    added spanish language support

--- app/src/main/java/com/example/buttoncounter/MainActivity.java
+++ app/src/main/java/com/example/buttoncounter/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.buttoncounter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private buttonIncre incrementalCounter;
+    private String locLang;
     private TextView viewCount;
+    private Button downIncre;
+    private Button upIncre;
+    private Button reset;
+    private Switch langSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +31,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         viewCount = (TextView) this.findViewById(R.id.count);
         incrementalCounter = new buttonIncre();
         viewCount.setText(String.valueOf(incrementalCounter.increment));
+        locLang = "en";
+        downIncre = (Button) this.findViewById(R.id.downButton);
+        upIncre = (Button) this.findViewById(R.id.upButton);
+        reset = (Button) this.findViewById(R.id.resetButton);
+        langSwitch = (Switch) this.findViewById(R.id.languageButton);
+        //downIncre.setText(R.string.downButText);
+        //upIncre.setText(R.string.upButText);
+        //reset.setText(R.string.resetButton);
+        //langSwitch.setText(R.string.languageButton);
 
     }
     //Restore state of app after change from saved state
@@ -26,10 +48,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onRestoreInstanceState(savedInstanceState);
         incrementalCounter.increment = savedInstanceState.getInt("count");
         viewCount.setText(String.valueOf(incrementalCounter.increment));
+        locLang = savedInstanceState.getString("locLang");
+        if(locLang.equals("en")) {
+            setLocale(this, "en");
+            locLang = "en";
+        }
+        else {
+            setLocale(this, "es");
+            locLang = "es";
+        }
+        resetText();
     }
     //Save state of app prior to change
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString("locLang", locLang);
         outState.putInt("count", incrementalCounter.increment);
         super.onSaveInstanceState(outState);
 
@@ -46,7 +79,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if(v.getId() == R.id.resetButton) {
             incrementalCounter.reset();
         }
+        else if(v.getId() == R.id.languageButton) {
+            if(locLang.equals("en")) {
+                setLocale(this, "es");
+                locLang = "es";
+            }
+            else {
+                setLocale(this, "en");
+                locLang = "en";
+            }
+            resetText();
+        }
         viewCount.setText(String.valueOf(incrementalCounter.increment));
+    }
+
+    private static void setLocale(Activity activity, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+    }
+    private void resetText() {
+        downIncre.setText(R.string.downButText);
+        upIncre.setText(R.string.upButText);
+        reset.setText(R.string.resetButton);
+        langSwitch.setText(R.string.languageButton);
     }
 
     private class buttonIncre {
